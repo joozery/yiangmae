989af7d746c107c875215fdfc95f53006978c2d7
joozery <136419786+joozery@users.noreply.github.com>
2025-03-24 06:19:31 +0700

    ✅ Fix GET /products iterable error

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -46,7 +46,9 @@ export default function CartPage() {
 
   return (
     <div className="max-w-4xl mx-auto p-6 font-prompt">
-      <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">ตะกร้าสินค้า</h2>
+      <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
+        ตะกร้าสินค้า
+      </h2>
 
       {loading ? (
         <p className="text-center">กำลังโหลด...</p>
